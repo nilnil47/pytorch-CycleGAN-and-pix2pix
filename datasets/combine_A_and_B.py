e38395095b8f7aaa4d1f4945abe52a447855a722
nilnil47 <nilnil47@gmail.com>
2023-04-13 13:43:15 +0300

    fix cobine_ab

--- datasets/combine_A_and_B.py
+++ datasets/combine_A_and_B.py
@@ -6,19 +6,27 @@ from multiprocessing import Pool
 
 
 def image_write(path_A, path_B, path_AB):
-    im_A = cv2.imread(path_A, 1) # python2: cv2.CV_LOAD_IMAGE_COLOR; python3: cv2.IMREAD_COLOR
-    im_B = cv2.imread(path_B, 1) # python2: cv2.CV_LOAD_IMAGE_COLOR; python3: cv2.IMREAD_COLOR
+    # python2: cv2.CV_LOAD_IMAGE_COLOR; python3: cv2.IMREAD_COLOR
+    im_A = cv2.imread(path_A, 1)
+    # python2: cv2.CV_LOAD_IMAGE_COLOR; python3: cv2.IMREAD_COLOR
+    im_B = cv2.imread(path_B, 1)
     im_AB = np.concatenate([im_A, im_B], 1)
     cv2.imwrite(path_AB, im_AB)
 
 
 parser = argparse.ArgumentParser('create image pairs')
-parser.add_argument('--fold_A', dest='fold_A', help='input directory for image A', type=str, default='../dataset/50kshoes_edges')
-parser.add_argument('--fold_B', dest='fold_B', help='input directory for image B', type=str, default='../dataset/50kshoes_jpg')
-parser.add_argument('--fold_AB', dest='fold_AB', help='output directory', type=str, default='../dataset/test_AB')
-parser.add_argument('--num_imgs', dest='num_imgs', help='number of images', type=int, default=1000000)
-parser.add_argument('--use_AB', dest='use_AB', help='if true: (0001_A, 0001_B) to (0001_AB)', action='store_true')
-parser.add_argument('--no_multiprocessing', dest='no_multiprocessing', help='If used, chooses single CPU execution instead of parallel execution', action='store_true',default=False)
+parser.add_argument('--fold_A', dest='fold_A', help='input directory for image A',
+                    type=str, default='../dataset/50kshoes_edges')
+parser.add_argument('--fold_B', dest='fold_B', help='input directory for image B',
+                    type=str, default='../dataset/50kshoes_jpg')
+parser.add_argument('--fold_AB', dest='fold_AB',
+                    help='output directory', type=str, default='../dataset/test_AB')
+parser.add_argument('--num_imgs', dest='num_imgs',
+                    help='number of images', type=int, default=1000000)
+parser.add_argument('--use_AB', dest='use_AB',
+                    help='if true: (0001_A, 0001_B) to (0001_AB)', action='store_true')
+parser.add_argument('--no_multiprocessing', dest='no_multiprocessing',
+                    help='If used, chooses single CPU execution instead of parallel execution', action='store_true', default=False)
 args = parser.parse_args()
 
 for arg in vars(args):
@@ -27,9 +35,11 @@ for arg in vars(args):
 splits = os.listdir(args.fold_A)
 
 if not args.no_multiprocessing:
-    pool=Pool()
+    pool = Pool()
 
 for sp in splits:
+    if sp == '.DS_Store':
+        continue
     img_fold_A = os.path.join(args.fold_A, sp)
     img_fold_B = os.path.join(args.fold_B, sp)
     img_list = os.listdir(img_fold_A)
@@ -58,8 +68,10 @@ for sp in splits:
             if not args.no_multiprocessing:
                 pool.apply_async(image_write, args=(path_A, path_B, path_AB))
             else:
-                im_A = cv2.imread(path_A, 1) # python2: cv2.CV_LOAD_IMAGE_COLOR; python3: cv2.IMREAD_COLOR
-                im_B = cv2.imread(path_B, 1) # python2: cv2.CV_LOAD_IMAGE_COLOR; python3: cv2.IMREAD_COLOR
+                # python2: cv2.CV_LOAD_IMAGE_COLOR; python3: cv2.IMREAD_COLOR
+                im_A = cv2.imread(path_A, 1)
+                # python2: cv2.CV_LOAD_IMAGE_COLOR; python3: cv2.IMREAD_COLOR
+                im_B = cv2.imread(path_B, 1)
                 im_AB = np.concatenate([im_A, im_B], 1)
                 cv2.imwrite(path_AB, im_AB)
 if not args.no_multiprocessing:
